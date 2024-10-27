ba8d48023f45bd98021c23e328a85d8406984b02
ripan baidya <ripanbaidya7980@gmail.com>
2024-10-27 21:43:49 +0530

    math library code added

--- src/com/github/ripan/math/Div.java
+++ src/com/github/ripan/math/Div.java
@@ -1,4 +1,10 @@
 package com.github.ripan.math;
 
 public class Div {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 2;
+
+        System.out.println(num1 / num2);
+    }
 }
--- src/com/github/ripan/math/Sum.java
+++ src/com/github/ripan/math/Sum.java
@@ -1,4 +1,8 @@
 package com.github.ripan.math;
 
 public class Sum {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20;
+        System.out.println(num1 + num2);
+    }
 }
